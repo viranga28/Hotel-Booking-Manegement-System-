01c7df585db6b7a6bdb2789595adb945673fd6b4
Viranga Lakshani <104049331+viranga28@users.noreply.github.com>
2025-01-20 08:46:31 +0530

    Create Register.js

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import authService from '../services/authService';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setError(null);
+        try {
+            await authService.register(username, password);
+            navigate('/login')
+        } catch (err) {
+            setError(err.message || "Registration failed.");
+        }
+    };
+
+    return (
+        <div className="auth-container">
+            <h2>Register</h2>
+            {error && <p className="error-message">{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
