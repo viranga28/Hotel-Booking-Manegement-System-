8c176945484ec05a0a54f15daa97733ddd60ab29
Viranga Lakshani <104049331+viranga28@users.noreply.github.com>
2025-01-21 09:06:06 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator')
+
+// @route   POST api/auth/register
+// @desc    Register a new user
+// @access  Public
+router.post('/register', [
+    check('username', 'Username is required').not().isEmpty(),
+    check('password', 'Password is required').isLength({min: 6})
+],async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    const { username, password } = req.body;
+
+    try {
+        let user = await User.findOne({
+            username
+        });
+
+        if (user) {
+            return res.status(400).json({
+                msg: 'User already exists'
+            });
+        }
+
+        user = new User({
+            username,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET, {
+                expiresIn: 3600
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.json({
+                    token
+                });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   POST api/auth/login
+// @desc    Authenticate user & get token
+// @access  Public
+router.post('/login', [
+    check('username', 'Username is required').exists(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+    const {
+        username,
+        password
+    } = req.body;
+
+    try {
+        let user = await User.findOne({
+            username
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Invalid Credentials'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                msg: 'Invalid Credentials'
+            });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET, {
+                expiresIn: 3600
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.json({
+                    token
+                });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
